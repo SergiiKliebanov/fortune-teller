e513b0d47586d45bab3f95256df9217b3c688e66
Sergii Kliebanov <SergiiKliebanov@users.noreply.github.com>
2023-02-27 01:17:21 +0000

    Update index.php

--- index.php
+++ index.php
@@ -12,6 +12,7 @@ if(isset($_GET['message']) && $_GET['message'] != '') {
 
 $cow = Farm::create(\Cowsayphp\Farm\Cow::class);
 // echo $cow->say($text);
+echo "URL: " . $DATABASE_URL;
 
 try {
   $conn = new PDO($DATABASE_URL, $DATABASE_USERNAME, $DATABASE_PASSWORD);
