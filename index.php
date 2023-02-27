f50a0ce2fb33369637a4413f0854a2c932ca8229
Sergii Kliebanov <SergiiKliebanov@users.noreply.github.com>
2023-02-27 03:48:24 +0000

    Update index.php

--- index.php
+++ index.php
@@ -13,6 +13,29 @@ if(isset($_GET['message']) && $_GET['message'] != '') {
 $cow = Farm::create(\Cowsayphp\Farm\Cow::class);
 echo $cow->say($text);
 */
+
+echo "<table style='border: solid 1px black;'>";
+echo "<tr><th>donator</th><th>amount</th><th>curse_id</th></tr>";
+
+class TableRows extends RecursiveIteratorIterator {
+  function __construct($it) {
+    parent::__construct($it, self::LEAVES_ONLY);
+  }
+
+  function current() {
+    return "<td style='width:150px;border:1px solid black;'>" . parent::current(). "</td>";
+  }
+
+  function beginChildren() {
+    echo "<tr>";
+  }
+
+  function endChildren() {
+    echo "</tr>" . "\n";
+  }
+}
+
+
 $options = [
 	\PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => 'false' 
 ];
@@ -27,7 +50,14 @@ try {
 		  $options);
 	
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  echo "Connected successfully";
+  $stmt = $conn->prepare("SELECT donator, amount, curse_id FROM donations");
+  $stmt->execute();
+  $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+  foreach(new TableRows(new RecursiveArrayIterator($stmt->fetchAll())) as $k=>$v) {
+    echo $v;
+  }
 } catch(PDOException $e) {
-  echo "Connection failed: " . $e->getMessage();
+  echo "Error: " . $e->getMessage();
 }
+$conn = null;
+echo "</table>";
