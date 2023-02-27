48348206a70433d2a28c2d7537e415399a3927a6
Sergii Kliebanov <SergiiKliebanov@users.noreply.github.com>
2023-02-27 03:40:23 +0000

    Update index.php

--- index.php
+++ index.php
@@ -13,11 +13,21 @@ if(isset($_GET['message']) && $_GET['message'] != '') {
 $cow = Farm::create(\Cowsayphp\Farm\Cow::class);
 echo $cow->say($text);
 */
+$options = [
+	\PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => 'false' 
+];
 
-$conn = new mysqli($_ENV['DATABASE_URL'], $_ENV['DATABASE_USERNAME'], $_ENV['DATABASE_PASSWORD']);
-
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
+try {
+  // $conn = new PDO($_ENV['DATABASE_URL'], $_ENV['DATABASE_USERNAME'], $_ENV['DATABASE_PASSWORD'],$options);
+  $conn = new PDO("mysql:host=" . $_ENV['DATABASE_HOSTNAME'] . 
+		  ";dbname=" . $_ENV['DATABASE_DATABASE'] .
+		  ";port= . $_ENV['DATABASE_PORT'], 
+		  $_ENV['DATABASE_USERNAME'], 
+		  $_ENV['DATABASE_PASSWORD'],
+		  $options);
+	
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  echo "Connected successfully";
+} catch(PDOException $e) {
+  echo "Connection failed: " . $e->getMessage();
 }
-echo "Connected successfully";
