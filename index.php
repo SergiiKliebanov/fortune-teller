c13aec8cb63aed139f11ee07d32615b40285a4ca
Sergii Kliebanov <SergiiKliebanov@users.noreply.github.com>
2023-02-27 00:10:32 +0000

    Update index.php

--- index.php
+++ index.php
@@ -12,4 +12,10 @@ if(isset($_GET['message']) && $_GET['message'] != '') {
 
 $cow = Farm::create(\Cowsayphp\Farm\Cow::class);
 // echo $cow->say($text);
-echo $cow->say(${_self.PORT});
+
+$conn = new mysqli($db_host, $db_username, $db_password);
+
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
