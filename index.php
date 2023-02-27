2e1fa2b6d8cccea4b382e76f423ebdb623e0da0e
Sergii Kliebanov <SergiiKliebanov@users.noreply.github.com>
2023-02-27 00:58:16 +0000

    Update index.php

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ if(isset($_GET['message']) && $_GET['message'] != '') {
 $cow = Farm::create(\Cowsayphp\Farm\Cow::class);
 // echo $cow->say($text);
 
-$conn = mysqli_connect($DB_CONNECTION_STRING, $DB_USERNAME, $DB_PASSWORD);
+$conn = mysqli_connect($DATABASE_URL, $DATABASE_USERNAME, $DATABASE_PASSWORD);
 
 if (!$conn) {
   die("Connection failed: " . mysqli_connect_error());
